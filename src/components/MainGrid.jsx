3041a0b9769826a64540d4d48c7b90319ccf8dca
Ton nom d'utilisateur githubYannRZG <yann.rezigui@outlook.fr>
2024-11-28 14:22:29 +0100

    images

--- src/components/MainGrid.jsx
+++ src/components/MainGrid.jsx
@@ -10,6 +10,7 @@ import Logo from "../assets/Logo.png";
 import Tokens from "../assets/Tokens.png";
 import BackgroundUser from "../assets/background_user.png";
 import A from "../assets/A.png";
+import Preseason from "../assets/background-preseason.png";
 
 export default function MainGrid() {
   const navigate = useNavigate();
@@ -20,15 +21,14 @@ export default function MainGrid() {
       <div className="grid grid-cols-1 lg:grid-cols-3 lg:gap-4 px-8 min-h-[60vh]">
         {/* MY COLLECTION Card (Visible sur tous les écrans) */}
         <div className="bg-zinc-800 rounded-lg text-white flex h-full transform hover:scale-105 transition-transform duration-300 ease-in-out">
-          <button className="flex items-end h-full w-full p-4 rounded-lg shadow-md bg-center bg-no-repeat lg:bg-[url('/src/assets/items.png')] lg:bg-[length:80%] lg:bg-[top]" onClick={() => navigate("/collection")}>
+          <button
+            className="flex items-end h-full w-full p-4 rounded-lg shadow-md bg-center bg-no-repeat lg:bg-[url('/src/assets/items.png')] lg:bg-[length:80%] lg:bg-[top]"
+            onClick={() => navigate("/collection")}
+          >
             <div className="flex flex-row">
               {/* Afficher l'image contract.png uniquement sur tablette et mobile */}
               <div className="flex items-center lg:hidden">
-                <img
-                  src={Contract}
-                  alt="Contract"
-                  style={{ height: "58px" }}
-                />
+                <img src={Contract} alt="Contract" style={{ height: "58px" }} />
               </div>
               <div>
                 <h1 className="text-3xl sm:text-4xl md:text-3xl font-extrabold text-yellow-300 text-start">
@@ -45,7 +45,7 @@ export default function MainGrid() {
         {/* CALCULATORS Card (Visible uniquement sur grand écran) */}
         <div className="flex flex-col h-full space-y-4 hidden lg:flex">
           <button
-          onClick={() => navigate("/calculator")}
+            onClick={() => navigate("/calculator")}
             className="flex items-end p-4 rounded-lg shadow-md text-white basis-2/3 bg-contain bg-center transform hover:scale-105 transition-transform duration-300 ease-in-out"
             style={{
               backgroundImage: "url('/src/assets/background-preseason.png')",
@@ -56,11 +56,7 @@ export default function MainGrid() {
                 CALCULATORS
               </h1>
               <div className="flex items-center gap-2">
-                <img
-                  src={Logo}
-                  alt=""
-                  style={{ height: "10px" }}
-                />
+                <img src={Logo} alt="" style={{ height: "10px" }} />
                 <p className="text-sm text-start font-light ">
                   SIMULATE YOUR NEED
                 </p>
@@ -70,10 +66,10 @@ export default function MainGrid() {
 
           {/* FARMING Card (Visible uniquement sur grand écran) */}
           <button
-          onClick={() => navigate("/farming")}
+            onClick={() => navigate("/farming")}
             className="bg-zinc-900 p-4 rounded-lg shadow-md text-white basis-1/3 bg-center bg-cover transform hover:scale-105 transition-transform duration-300 ease-in-out"
             style={{
-              backgroundImage: "url('/src/assets/custom-league.png')",
+              backgroundImage: "url('/assets/custom-league.png')", // Chemin mis à jour
               backgroundSize: "cover",
               backgroundPosition: "center",
             }}
@@ -99,7 +95,10 @@ export default function MainGrid() {
 
         {/* DAILY and MONTHLY Cards (Visible sur tous les écrans) */}
         <div className="space-y-4 flex flex-col h-full">
-          <button onClick={() => navigate("/profile")} className="flex items-end h-1/6 p-2 rounded-lg shadow-md bg-zinc-800 text-white transform hover:scale-105 transition-transform duration-300 ease-in-out hidden lg:flex">
+          <button
+            onClick={() => navigate("/profile")}
+            className="flex items-end h-1/6 p-2 rounded-lg shadow-md bg-zinc-800 text-white transform hover:scale-105 transition-transform duration-300 ease-in-out hidden lg:flex"
+          >
             {/* Omiage Card (Visible uniquement sur grand écran) */}
             <div className="flex space-x-2">
               <div className="flex items-center">
@@ -130,7 +129,7 @@ export default function MainGrid() {
           </button>
 
           <button
-          onClick={() => navigate("/daily")}
+            onClick={() => navigate("/daily")}
             className="flex items-end flex-1 p-4 rounded-lg bg-zinc-800 shadow-md text-white transform hover:scale-105 transition-transform duration-300 ease-in-out"
             style={{
               backgroundImage: "url('/src/assets/rewards-pattern1.png')",
@@ -156,7 +155,7 @@ export default function MainGrid() {
           </button>
 
           <button
-          onClick={() => navigate("/monthly")}
+            onClick={() => navigate("/monthly")}
             className="flex items-end flex-1 p-4 rounded-lg bg-zinc-800 shadow-md text-white transform hover:scale-105 transition-transform duration-300 ease-in-out"
             style={{
               backgroundImage: "url('/src/assets/rewards-pattern2.png')",
@@ -183,11 +182,7 @@ export default function MainGrid() {
 
       {/* Logo */}
       <div className="flex items-center justify-center mt-6 opacity-70">
-        <img
-          src={A}
-          alt=""
-          style={{ height: "100px", width: "auto" }}
-        />
+        <img src={A} alt="" style={{ height: "100px", width: "auto" }} />
       </div>
     </>
   );
